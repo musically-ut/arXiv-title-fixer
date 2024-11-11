e8f3a3291506f2e7e1c404dc03506b969334e6a7
Utkarsh Upadhyay <musically.ut@gmail.com>
2024-11-11 10:32:24 +0100

    Remove tabs permission.

--- webext/background.js
+++ webext/background.js
@@ -11,20 +11,6 @@
     }
   }
 
-  // Listen for tab activation
-  chrome.tabs.onActivated.addListener(function (activeInfo) {
-    chrome.tabs.get(activeInfo.tabId, function (tab) {
-      updateActionButton(tab.id, tab.url);
-    });
-  });
-
-  // Listen for tab updates
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete") {
-      updateActionButton(tabId, tab.url);
-    }
-  });
-
   // Handle action button clicks
   chrome.action.onClicked.addListener(function (tab) {
     chrome.tabs.sendMessage(tab.id, { action: "update-page-title" });
